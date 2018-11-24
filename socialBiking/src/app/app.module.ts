b379040893464b7bc0fb62fe10be0e6aa7a4515c
mmmarzouki <mmmarzouki@gmail.com>
2018-11-24 21:41:17 +0000

    revert after add maps

--- socialBiking/src/app/app.module.ts
+++ socialBiking/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Component/navbar/navbar.component';
-import { PublishComponent } from './Component/publish/publish.component';
+import { PublishComponent } from './Component/home/publish/publish.component';
 import { EventsComponent } from './Component/events/events.component';
-import { PostsComponent } from './Component/posts/posts.component';
+import { PostsComponent } from './Component/home/posts/posts.component';
 import { MapComponent } from './Component/map/map.component';
 import { HomeComponent } from './Component/home/home.component';
 
